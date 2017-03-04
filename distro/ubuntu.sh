2fefb88701cfac6207677d865e81a59b33ccc29e
James <james@jnwarp.com>
2017-03-04 16:14:19 -0500

    Create ubuntu.sh

---
+++ distro/ubuntu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## Basic Setup
+sudo apt-get install git curl vim zsh screenfetch gnupg-curl ranger htop ufw fail2ban
+echo "America/New_York" > /etc/timezone
+
+## Security
+sed -i '
+s/.*PermitRootLogin.*/PermitRootLogin no/
+s/.*PasswordAuthentication.*/PasswordAuthentication no/
+s/.*AuthorizedKeysFile.*/AuthorizedKeysFile %h\/.ssh\/authorized_keys/
+' /etc/ssh/sshd_config
+
+echo "
+[DEFAULT]
+# 34 days
+bantime  = 3000000
+
+# 10 hours
+findtime = 36000
+
+[ssh]
+enabled = true
+
+[ssh-ddos]
+enabled  = true
+" > /etc/fail2ban/jail.local
